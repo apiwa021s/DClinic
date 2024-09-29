51d474810d8f5513d69df88e139ab35ba2000435
Nat <nattapon@gmail.com>
2024-09-29 11:49:29 +0700

    fix contact

--- src/app/ui/Widget/ContactInfoWidget.jsx
+++ src/app/ui/Widget/ContactInfoWidget.jsx
@@ -8,19 +8,19 @@ export default function ContactInfoWidget() {
         <i className="cs_accent_bg">
           <Icon icon="ep:location" />
         </i>
-        123 Anywhere St., Any City 12345
+        456/789 ถนนสุขุมวิท, แขวงคลองตัน, เขตวัฒนา, กรุงเทพฯ 10110
       </li>
       <li>
         <i className="cs_accent_bg">
           <Icon icon="fluent:call-24-regular" />
         </i>
-        123-456-7890
+        0616479191
       </li>
       <li>
         <i className="cs_accent_bg">
           <Icon icon="bi:envelope" />
         </i>
-        hellocallcenter@gmail.com
+        contact@dbangkokclinic.com
       </li>
     </ul>
   );
